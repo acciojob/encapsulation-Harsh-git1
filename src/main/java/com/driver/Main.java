5a8ad5a17172b957794a3a525d84667919f105a3
Harsh Srivastava <srivastavaharsh8957@gmail.com>
2023-08-17 13:10:45 +0530

    encapsulation sol

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        // obj.name = "Harsh";
+        // System.out.println(obj.name);
+        // as name has a private access modifier so direct access or setting it will show error, so
+        // we have to use getter and setter to access and setting the name variable
+
+        obj.setName("Harsh");
+        System.out.println(obj.getName());
+
+    }
 }
